802f65cc032741a59de31cc7678d1b5923271981
root <akashkarmakar787@gmail.com>
2019-07-31 19:16:30 +0000

    added all files

---
+++ app.js
@@ -0,0 +1,55 @@
+var express=require("express");
+var app=express();
+var nodemailer = require('nodemailer');
+app.set("view engine","ejs");
+
+var bodyparser=require("body-parser");
+app.use(bodyparser.urlencoded({extended:false}));
+//=========================
+//MAILING ROUTES
+//=========================
+
+                // create reusable transporter object using SMTP transport 
+                var transporter = nodemailer.createTransport({
+                    service: 'Gmail',
+                    auth: {
+                        user: 'aroy0761@gmail.com',
+                        pass: 'sayankarmakar'
+                    }
+                });
+app.get("/signup",function(req,res){
+	res.render("signup");
+});
+app.post("/credentials",function(req,res){
+	var otp=Math.floor((Math.random() * 100) + 54);
+	var mailOptions = {
+		
+                    from: 'aroy0761@gmail.com', // sender address 
+                    to: req.body.email, // list of receivers 
+                    subject: 'QUIZINE OTP', // Subject line 
+                    html: 'Your one time password is : '+otp  // html body 
+                };
+
+                transporter.sendMail(mailOptions, function (err, info) {
+                    if(err){console.log(err);}
+					else {
+						
+						console.log(otp);
+						res.render("checkotp",{otp:otp});
+					}
+              });
+	
+	
+});
+app.post("/checkotp",function(req,res){
+	if(req.body.actualotp==req.body.otp){
+		res.send("you did it");
+	}
+    else{
+		res.render("checkotp",{otp:req.body.actualotp});
+	}
+});
+app.get("/",function(req,res){res.render("index");});
+app.listen(3000,function(){
+console.log("App started");
+});
---
+++ index.js
@@ -0,0 +1,45 @@
+var express=require('express');
+var nodemailer = require("nodemailer");
+var app=express();
+/*
+    Here we are configuring our SMTP Server details.
+    STMP is mail server which is responsible for sending and recieving email.
+*/
+var smtpTransport = nodemailer.createTransport("SMTP",{
+    service: "Gmail",
+    auth: {
+        user: "aroy0761@gmail.com",
+        pass: "sayankarmakar"
+    }
+});
+var rand,mailOptions,host,link;
+/*------------------SMTP Over-----------------------------*/
+
+/*------------------Routing Started ------------------------*/
+
+app.get('/',function(req,res){
+    res.sendfile('index.html');
+});
+app.get('/send',function(req,res){
+        rand=Math.floor((Math.random() * 100) + 54);
+    host=req.get('host');
+    link="http://"+req.get('host')+"/verify?id="+rand;
+    mailOptions={
+        to : "akashkarmakar787@gmail.com",
+        subject : "Please confirm your Email account",
+        html : "Hello,<br> Please Click on the link to verify your email.<br><a href="+link+">Click here to verify</a>" 
+    };
+    console.log(mailOptions);
+    smtpTransport.sendMail(mailOptions, function(error, response){
+     if(error){
+            console.log(error);
+        res.end("error");
+     }else{
+            console.log("Message sent: " + response.message);
+        res.end("sent");
+         }
+});
+});
+app.listen(3000,function(){
+    console.log("Express Started on Port 3000");
+});
